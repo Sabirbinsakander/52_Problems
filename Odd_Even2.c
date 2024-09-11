e7febb4298275cfd86e1102de22ecd65481d66b3
Sabir <sabirbinsakender@gmail.com>
2024-09-12 00:37:10 +0600

    Start problem solving journy& this my first commit

---
+++ Odd_Even2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int t, i;
+    char n [101];
+    scanf("%d", &T);
+    for (i = 1; i <= T; i++)
+    {
+        scanf("%s", &n);
+    }
+    
+    return 0;
+}
+
+
